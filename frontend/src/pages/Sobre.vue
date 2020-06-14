ac19026a91f15000245b808861da9121bfe9ca6f
Luis Leao <luis.leao@gmail.com>
2020-06-14 17:15:52 -0300

    ajuste sobre

--- frontend/src/pages/Sobre.vue
+++ frontend/src/pages/Sobre.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
     <div class="q-px-lg">
-      <h4 class="q-mb-lg q-mt-none">Sobre o projeto</h4>
+      <h4 class="q-mb-lg q-mt-lg q-mb-sm">Sobre o projeto</h4>
       <p>O Durma Bem Caminhoneiro é uma solução inovadora, pois os caminhoneiros poderão acessar nosso webapp e pesquisar locais úteis que existe na rodovia, como: pontos de apoio da CCR, restaurantes, postos de combustível e outros.</p>
       <p>O webapp também permite a reserva de uma vaga para passar a noite em algum Ponto de Parada e Descanso credenciado.</p>
       <p>O Durma Bem Caminhoneiro tem como objetivo prover mais qualidade de vida para o caminhoneiro, através da instalação de containers limpos e padronizados onde ele pode tomar banho e ficar tranquilo durante o seu momento de descanso.</p>
