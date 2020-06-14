8d142a10daafa4ac5c4f4c3edd47105572037f70
adrielrenato <39914516+adrielrenato@users.noreply.github.com>
2020-06-14 01:17:35 -0300

    Listar reservas do motorista

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -85,55 +85,17 @@ export default {
     updateUser () {
       this.token = JSON.parse(this.$q.localStorage.getItem('token')) || {}
     },
-    getBookings () {
+    async getBookings () {
+      var config = {
+        headers: { Authorization: 'Bearer ' + this.token.access_token }
+      }
       // TODO: sair se não estiver autenticado
       // TODO: chamada no servidor
       // https://api.durmabemcaminhoneiro.com.br/api/reservas
-      console.log('carregar dados servidor')
-      this.bookings = [
-        {
-          uuid: 'db49c992-01f8-401e-8178-466007566d7f',
-          data_chegada_em: '2020-06-14 21:00:00',
-          data_saida_em: '2020-06-15 08:00:00',
-          nome: 'Teste',
-          estado_uf: 'SP',
-          cidade_nome: 'Itu',
-          rodovia: 'BR-116',
-          km: '500',
-          valor_estadia: 50,
-          tags: {
-            durma_bem_caminhoneiro: true,
-            apoio_ccr: true,
-            restaurante: true,
-            abastecimento: true,
-            chuveiro: false,
-            dormir: false
-          },
-          latitude: '-23.2847281',
-          longitude: '-47.27839300000001'
-        },
-        {
-          uuid: '7f03b8c8-6e28-4783-843c-4369aa562501',
-          data_chegada_em: '2020-06-14 21:00:00',
-          data_saida_em: '2020-06-15 08:00:00',
-          nome: 'Teste',
-          estado_uf: 'SP',
-          cidade_nome: 'Itu',
-          rodovia: 'BR-116',
-          km: '500',
-          valor_estadia: 50,
-          tags: {
-            durma_bem_caminhoneiro: true,
-            apoio_ccr: true,
-            restaurante: true,
-            abastecimento: true,
-            chuveiro: false,
-            dormir: false
-          },
-          latitude: '-23.2847281',
-          longitude: '-47.27839300000001'
-        }
-      ]
+      this.bookings = this.$axiso.get('https://api.durmabemcaminhoneiro.com.br/api/reservas', { headers: config })
+        .then((response) => {
+          return response || []
+        })
     }
   }
 
